8fcf4b0d0a30e107660426c5479f7df6b7ccd26c
fmuniente <frd.muniente@gmail.com>
2021-07-22 21:02:20 +0200

    .

--- server/app.js
+++ server/app.js
@@ -22,6 +22,8 @@ app.use('/api/auth', authRoutes);
 const indexRoutes = require('./routes/index');
 app.use('/api', indexRoutes);
 
+app.use((req, res) => res.sendFile(__dirname + '/public/index.html'));
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 

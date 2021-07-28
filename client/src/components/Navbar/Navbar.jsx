917ac7bb56ea93bff5990f0b9e2fa87d27b5ecba
Miki-Geoghegan <mikigeoghegan@gmail.com>
2021-07-28 19:57:22 +0200

    latest

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,51 +1,32 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import Hamburger from './Hamburger'
 
-export default function Navbar(props){
+const Navbar = (props) => {
 	const {user} = props
-	
-	const [hamburgerOpen, setHamburgerOpen] = useState(false);
 
-	const toggleHamburger = () =>{
-		setHamburgerOpen(!hamburgerOpen)
-	}
 	return (
-		<div>
-			<div className="navigation">
-				<Link to={'/'} className='nav__projectName'>BestReads</Link>
-				<ul>
-					<li><Link to={`/auth/profile/user`} className='authLink'>User page</Link></li>
-					<li><Link to={'/search'} className='authLink'>Search book</Link></li>
-					<li><Link to={'/ebook/create'} className='authLink'>Create an eBook</Link></li>
-					<li><button className='nav-logoutbtn' onClick={props.handleLogout}>Logout</button></li>
-				</ul>
-				<div className="hamburger" onClick={toggleHamburger}>
-					<Hamburger />
-				</div>
-
-			</div>
-
-			
-		{/* 		<Link to={'/'} className='nav__projectName'>
+		<nav>
+			<Link to={'/'} className='nav__projectName'>
 				BestReads
-				</Link>
+			</Link>
 
 			<div className='nav__authLinks'>
 				{props.user ? (
 					<>
-			<div className="navigation">
-				<Link to={'/'} className='nav__projectName'>BestReads</Link>
-				<ul>
-					<li><Link to={`/auth/profile/user`} className='authLink'>User page</Link></li>
-					<li><Link to={'/search'} className='authLink'>Search book</Link></li>
-					<li><Link to={'/ebook/create'} className='authLink'>Create an eBook</Link></li>
-					<li><button className='nav-logoutbtn' onClick={props.handleLogout}>Logout</button></li>
-				</ul>
-				<div className="hamburger" onClick={toggleHamburger}>
-					<Hamburger />
-				</div>
+						<Link to={'/search'} className='authLink'>
+							Search book
+						</Link>
+						<Link to={'/ebook/create'} className='authLink'>
+							Create an eBook
+						</Link>
+						<Link to={`/auth/profile/user`} className='authLink'>
+							User page
+						</Link>
+
+						<button className='nav-logoutbtn' onClick={props.handleLogout}>
+							Logout
+						</button>
 					</>
 				) : (
 					<>
@@ -59,11 +40,11 @@ export default function Navbar(props){
 							Create Ebook
 						</Link>
 					</>
-				)}*/}
+				)}
 			</div>
-
-
-
+		</nav>
 	);
 };
+
+export default Navbar;
 

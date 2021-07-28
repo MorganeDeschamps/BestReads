1f58e80a7794df0129a64538fdc113d79774b31e
Miki-Geoghegan <mikigeoghegan@gmail.com>
2021-07-28 03:19:36 +0200

    small edit post merge

--- client/src/components/Book/BookEbook.jsx
+++ client/src/components/Book/BookEbook.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import {getOneBook} from "../services/api.services"
+import {getOneBook} from "../../services/api.services"
 
 
 
